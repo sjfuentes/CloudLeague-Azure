7c7da0f1df3e1fa55702717de05b11375132f1ec
sjfuentes <sjfuentes@outlook.com>
2022-12-14 16:05:14 -0500

    creating the DB

--- modules/db/mainDB.tf
+++ modules/db/mainDB.tf
@@ -1,5 +1,15 @@
 resource "azurerm_sql_server" "app_DB_server"{
-    name = "app_DB_server01020"
+    name = "app-db-server1256"
     resource_group_name = "1-55a4b655-playground-sandbox"
-    location = 
+    location = var.location
+    version = "12.0"
+    administrator_login          = "4dm1n157r470r"
+    administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_sql_database" "app-DB" {
+  name = "app-db"
+  resource_group_name = "1-80cab9fc-playground-sandbox"
+  location = var.location
+  server_name = azurerm_sql_server.app_DB_server.name
 }
--- modules/virtualNetwork/mainVN.tf
+++ modules/virtualNetwork/mainVN.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "vnet" {
     name = "CloudLeague-vnet"
     address_space = [ "10.0.0.0/16" ]
     location = var.location
-    resource_group_name = "1-55a4b655-playground-sandbox"
+    resource_group_name = "1-80cab9fc-playground-sandbox"
     tags = {
       Environment = "test"
       Team = "Cloud-League"
